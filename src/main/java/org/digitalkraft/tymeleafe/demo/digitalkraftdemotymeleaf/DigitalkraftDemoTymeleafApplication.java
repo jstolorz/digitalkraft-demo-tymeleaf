b323e6827bed60834dbf294d69f139d305ce170d
jstolorz <jstolorz@gmail.com>
2017-11-22 19:36:40 +0100

    Init Application

---
+++ src/main/java/org/digitalkraft/tymeleafe/demo/digitalkraftdemotymeleaf/DigitalkraftDemoTymeleafApplication.java
@@ -0,0 +1,12 @@
+package org.digitalkraft.tymeleafe.demo.digitalkraftdemotymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalkraftDemoTymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalkraftDemoTymeleafApplication.class, args);
+	}
+}
---
+++ src/test/java/org/digitalkraft/tymeleafe/demo/digitalkraftdemotymeleaf/DigitalkraftDemoTymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package org.digitalkraft.tymeleafe.demo.digitalkraftdemotymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DigitalkraftDemoTymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
